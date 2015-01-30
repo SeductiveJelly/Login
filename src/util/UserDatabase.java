876903639bb771c10c76f08bfd99db44f9e4df6d
William <raywilliam1212@yahoo.com>
2015-01-29 20:23:26 -0600

    Added some username and password checking

--- src/util/UserDatabase.java
+++ src/util/UserDatabase.java
@@ -1,35 +1,56 @@
 package util;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserDatabase {
 	private ArrayList<User> users;
-	
+
 	public UserDatabase() {
 		users = new ArrayList<User>();
 	}
-	
-	// TODO: Fix
+
+	// TODO: Finish
 	public boolean createUser(String username, String password, String firstName, String lastName) {
 		return true;
 	}
 	
+	/**
+	 * Checks a regex expression against a string
+	 */
+	public boolean regexChecker(String regex, String text) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(text);
+		
+		return matcher.find();
+
+	}
+
 	public boolean isValidUsername(String username) {
-		boolean valid = false;
+		// Defines the regex pattern for a username
+		String regex = "^(?=[^#%^&+\\/]+$)(?=\\S+$).{5,15}$";
 		
-		// Make sure the username is unique 
-		if(!(isUser(username))) {
-			
-			// Is the username between 5-15 characters in length
-			if(username.length() <= 15 && username.length() >= 5) {
-				
-				valid = true;
-				
-			}
-			
+		// Make sure the username is unique
+		if (!(isUser(username))) {
+
+			if(regexChecker(regex, username)) return true;
+			else System.err.println("Error: Username must be in defined parameters!");
 		}
+		else System.err.println("Error: Username already exsits!");
+
+		return false;
 		
-		return valid;
+	}
+	
+	public boolean isValidPassword(String password) {
+		// Defines the invalid chars for a password
+		String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,20}$";
+		
+		if(regexChecker(regex, password)) return true;
+		else System.err.println("Error: Password must be in defined parameters!");
+		
+		return false;
 		
 	}
 	
